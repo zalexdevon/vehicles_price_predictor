6005d1c8ca8f8f39b3a38e0dd6678dea691b8a8f
Minh Tam <trantam2004hocjava@gmail.com>
2025-06-03 14:32:06 +0700

    update dc1

--- src/data_correction/dc1.py
+++ src/data_correction/dc1.py
@@ -7,30 +7,6 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
 
 
-special_chars = ["#", "*", "x"]
-
-
-def is_not_valid(value):
-    if pd.isnull(value):
-        return False
-
-    for char in special_chars:
-        if char in value:
-            return True
-
-    return False
-
-
-def replace_not_valid_value_by_nan(col):
-    index_not_valid = col.index[col.apply(is_not_valid)]
-    col[index_not_valid] = np.nan
-    return col
-
-
-def replace_value_l0_by_nan(col):
-    value_l0_index = col.index[col < 0]
-    col[value_l0_index] = np.nan
-    return col
 
 
 class BeforeTransformer(BaseEstimator, TransformerMixin):
@@ -45,50 +21,38 @@ class BeforeTransformer(BaseEstimator, TransformerMixin):
         df = X
 
         # Xóa các cột không liên quan
-        df = df.drop(columns=["time", "station", "AMB_TEMP", "time_datetime"])
-
-        # Xóa các cột có tỉ lệ missing lớn
-        ## Trong các cột (ngoại trừ cột mục tiêu), thay thế tất cả các giá trị mà có chứa 1 trong 3 kí tự đặc biệt sau: ['#', '*', 'x'] thành np.nan
-        special_chars = ["#", "*", "x"]
-        target_col = "temperature_cat"
-        feature_cols = list(set(df.columns.tolist()) - set([target_col]))
-        df[feature_cols] = df[feature_cols].apply(replace_not_valid_value_by_nan)
-        df["CH4"].unique()
-
-        ## Các cột có tỉ lệ missing lớn > 10% là: UVB, PH_RAIN, RAIN_COND, NMHC, CH4, THC
-        df = df.drop(columns=["UVB", "PH_RAIN", "RAIN_COND", "NMHC", "CH4", "THC"])
+        df = df.drop(
+            columns=[
+                "vin", 'sellingprice', 'saledate'
+            ]
+        )
 
         # Đổi tên cột
         rename_dict = {
-            "CO": "CO_num",
-            "NO": "NO_num",
-            "NO2": "NO2_num",
-            "NOx": "NOx_num",
-            "O3": "O3_num",
-            "PM10": "PM10_num",
-            "PM2.5": "PM2_5_num",
-            "RAINFALL": "RAINFALL_num",
-            "RH": "RH_num",
-            "SO2": "SO2_num",
-            "WD_HR": "WD_HR_num",
-            "WIND_DIREC": "WIND_DIREC_num",
-            "WIND_SPEED": "WIND_SPEED_num",
-            "WS_HR": "WS_HR_num",
-            "temperature_cat": "temperature_cat_target",
+            "year": "year_nom",
+            "make": "make_nom",
+            "model": "model_nom",
+            "trim": "trim_nom",
+            "body": "body_nom",
+            "transmission": "transmission_nom",
+            "state": "state_nom",
+            "condition": "condition_numcat",
+            "odometer": "odometer_num",
+            "color": "color_nom",
+            "interior": "interior_nom",
+            "seller": "seller_nom",
+            "mmr": "mmr_num",
+            "sellingprice_cat": "sellingprice_cat_target",
+
+
         }
+
 
         df = df.rename(columns=rename_dict)
 
         # Sắp xếp các cột theo đúng thứ tự
-        (
-            numeric_cols,
-            numericCat_cols,
-            cat_cols,
-            binary_cols,
-            nominal_cols,
-            ordinal_cols,
-            target_col,
-        ) = myfuncs.get_different_types_cols_from_df_4(df)
+        numeric_cols, numericCat_cols, cat_cols, binary_cols, nominal_cols, ordinal_cols, target_col = myfuncs.get_different_types_cols_from_df_4(df)
+
 
         df = df[
             numeric_cols
@@ -99,26 +63,7 @@ class BeforeTransformer(BaseEstimator, TransformerMixin):
             + [target_col]
         ]
 
-        # Kiểm tra kiểu dữ liệu các cột
-        feature_cols = list(set(df.columns.tolist()) - set([target_col]))
 
-        ## Thay thế các giá trị 'NR' trong cột RAINFALL_num bằng 0
-        col_name = "RAINFALL_num"
-        replace_list = [(["NR"], 0)]
-        df[col_name] = myfuncs.replace_in_series_33(df[col_name], replace_list)
-
-        ## Thay thế các giá trị 'NR' trong cột PM2_5_num bằng 0
-        col_name = "PM2_5_num"
-        replace_list = [(["NR"], 0)]
-        df[col_name] = myfuncs.replace_in_series_33(df[col_name], replace_list)
-
-        ## Chuyển các cột sai dữ liệu về kiểu dữ liệu float
-        df[feature_cols] = df[feature_cols].astype("float32")
-
-        # Kiểm tra nội dung các cột numeric
-        ## Thay thế các giá trị âm trong các cột này thành np.nan
-        col_names = ["NO_num", "NO2_num", "O3_num", "SO2_num"]
-        df[col_names] = df[col_names].apply(replace_value_l0_by_nan)
 
         return df
 
