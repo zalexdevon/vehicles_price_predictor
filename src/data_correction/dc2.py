fae4ff72aa3489bca7843d2cf6ff1c655b1a6f3a
Minh Tam <trantam2004hocjava@gmail.com>
2025-06-03 15:19:28 +0700

    update dc2

--- src/data_correction/dc2.py
+++ src/data_correction/dc2.py
@@ -1,10 +1,4 @@
-import pandas as pd
-from Mylib import myfuncs
-from sklearn.compose import ColumnTransformer
-from sklearn.impute import SimpleImputer
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.pipeline import Pipeline
-import data_correction.dc1 as dc1
 
 
 class TransformerOnTrainAndTest(BaseEstimator, TransformerMixin):
